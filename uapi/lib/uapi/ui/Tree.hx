e6b6667b6747b3c22f65deb0439c0cfe771974af
tecteun <tecteun@hotmail.com>
2019-04-10 14:02:52 +0200

    update tree logic

--- uapi/lib/uapi/ui/Tree.hx
+++ uapi/lib/uapi/ui/Tree.hx
@@ -7,16 +7,34 @@ import js.Browser;
 @:expose("Tree")
 @:build(Macros.buildInlineDom(["node", "styles"]))
 class Tree extends js.html.DOMElement{
-    static var node = <div onclick="if(event.target == this) this.firstElementChild.classList.toggle('collapsed'); return false;" class="::_class::" style="width: 100px;">        
+    static var node = <div onclick="if(event.target == this) this.classList.toggle('collapsed'); return false;" 
+                           class="::_class::">
         ::key::
         <section>::value::</section>
     </div>;
     static var styles = <style>
-         .collapsed {
+         .collapsed section{
              height: 0px;
          }
+         .treenode::before {
+            content: '▲';
+            transform: rotate(180deg);
+            position: absolute;
+            left: -15px;
+            transition: transform 220ms ease-out;
+        }
+        .treenode.collapsed::before {
+            transform: rotate(90deg);
+        }
+         .treenode {
+             margin-left: 20px;
+             display: flex;
+             justify-content: space-between;
+             position: relative;
+             border-bottom: 1px solid grey;
+         }
          .treenode section {
-             background:pink;
+             background:honeydew;
              overflow:hidden;
          }
         </style>;
@@ -39,6 +57,7 @@ class Tree extends js.html.DOMElement{
         walk =  function(obj, base, ?depth=0) {
             var sections:js.html.HTMLCollection = base.getElementsByTagName("section");
             base = (sections.length > 0 ? sections.item(0) : base);
+            base.style.marginLeft = (100 * depth) + "px";
             var fields = Reflect.fields(obj);
             var cls = Type.getClass(obj);
             if(depth > maxDepth)

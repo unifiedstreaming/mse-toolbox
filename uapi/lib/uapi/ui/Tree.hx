33658a8a6bfc1f1f10e4428535f478e01fb9743a
tecteun <tecteun@hotmail.com>
2019-04-10 10:10:25 +0200

    max parsing depth set to 3

--- uapi/lib/uapi/ui/Tree.hx
+++ uapi/lib/uapi/ui/Tree.hx
@@ -7,7 +7,7 @@ import js.Browser;
 @:keep
 @:expose("Tree")
 @:build(Macros.buildInlineDom(["node", "styles"]))
-class Tree {
+class Tree extends js.html.DOMElement{
     static var node = <div onclick="if(event.target == this) this.firstElementChild.classList.toggle('collapsed'); return false;" class="::_class::" style="width: 100px;">        
         ::key::
         <section>::value::</section>
@@ -23,7 +23,7 @@ class Tree {
         </style>;
 
     private static inline var ID = "mse-toolbox-tree-";
-    public function new(obj:Dynamic):Void {
+    public static function create(obj:Dynamic):js.html.DOMElement {
         obj = obj == null ? {
             aa: 1,
             bb: [ 2,3,4 ],
@@ -35,11 +35,18 @@ class Tree {
             }
         } : obj;
         var base:js.html.DOMElement = cast Browser.document.createDivElement();
-        var walk:Dynamic->js.html.DOMElement->Void = null;
-        walk =  function(obj, base) {
+        var walk:Dynamic->js.html.DOMElement->?Int->Void = null;
+        var maxDepth = 3;
+        walk =  function(obj, base, ?depth=0) {
             var sections:js.html.HTMLCollection = base.getElementsByTagName("section");
             base = (sections.length > 0 ? sections.item(0) : base);
-            for(o in Reflect.fields(obj)){
+            var fields = Reflect.fields(obj);
+            var cls = Type.getClass(obj);
+            if(depth > maxDepth)
+                return;
+            if(cls != null)
+                fields = fields.concat(Type.getInstanceFields(cls));
+            for(o in fields){
                 var field = Reflect.field(obj, o);
                 if(!Std.is(field, Array) && !Std.is(field, String) && !Std.is(field, Bool) && !Std.is(field, Int) && !Std.is(field, Float)){
                     
@@ -51,8 +58,8 @@ class Tree {
                                                  value: ''
                                                }
                                               )
-                                          )
-                        );
+                                          ),
+                        depth++);
                 }else{
                     
                     base.appendChild(node({ _class: "treenode", key: '${o}', value:'${Json.stringify(field)}'}));
@@ -61,31 +68,6 @@ class Tree {
         }
         walk(obj, base);
         Browser.document.body.appendChild(styles({}));
-        Browser.document.body.appendChild(base);
-    }
-    static function test(data:Dynamic):js.html.DOMElement{
-        var base = Browser.document.createDivElement();
-        base.className = '${ID}column';
-        for(f in Reflect.fields(data)){
-            var sub = Reflect.field(data, f);
-            var _key = Browser.document.createDivElement();
-            _key.innerText = 'key: $f';
-            var el = Browser.document.createDivElement();
-            el.className = '${ID}row';
-            el.appendChild(_key);
-            switch(Type.typeof(sub)){
-                
-                case TObject: 
-                    el.appendChild(test(sub));
-                default:
-                    var _value = Browser.document.createDivElement();
-                    _value.innerText = 'value: $sub';
-                    el.appendChild(_value);
-            }
-            base.appendChild(el);
-            
-        }
-
         return base;
     }
 }
